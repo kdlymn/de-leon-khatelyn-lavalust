55aeaaf1defc0a4a76f797f9a2b7506bbde18fdf
Khate Lyn De Leon <khatelyndeleon@gmail.com>
2026-09-09 23:52:12 +0800

    Provide student profile data

--- app/controllers/StudentController.php
+++ app/controllers/StudentController.php
@@ -18,6 +18,10 @@ class StudentController extends Controller
             'year'       => '3rd Year',
             'section'    => '3F3',
             'email'      => 'jayoffepascua@gmail.com',
+            'skills'     => ['PHP', 'MySQL', 'HTML & CSS'],
+            'hobbies'    => ['Reading', 'Music', 'Gaming'],
+            'contact_number' => '0912 345 6789',
+            'address'    => 'Davao City, Philippines',
         ];
 
         $this->call->view('student_profile', $data);
