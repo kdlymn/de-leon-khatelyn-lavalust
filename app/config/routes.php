c6256574de602a80182798e7f7fb8349340f864d
Khate Lyn De Leon <khatelyndeleon@gmail.com>
2026-09-09 22:58:37 +0800

    crud

--- app/config/routes.php
+++ app/config/routes.php
@@ -47,3 +47,16 @@ defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 $router->get('/', 'StudentController::index', ['middleware' => 'StudentMiddleware']);
 
 $router->get('/show-users', 'UserController::showUsers');
+
+$router->get('/login', 'AuthController::login');
+$router->post('/login', 'AuthController::authenticate');
+$router->get('/register', 'AuthController::register');
+$router->post('/register', 'AuthController::store_register');
+$router->get('/logout', 'AuthController::logout');
+
+$router->get('/products', 'ProductController::index')->middleware('auth');
+$router->get('/products/create', 'ProductController::create')->middleware(['auth', 'admin']);
+$router->post('/products/create', 'ProductController::store')->middleware(['auth', 'admin']);
+$router->get('/products/edit/{id}', 'ProductController::edit')->middleware(['auth', 'admin'])->where_number('id');
+$router->post('/products/edit/{id}', 'ProductController::update')->middleware(['auth', 'admin'])->where_number('id');
+$router->post('/products/delete/{id}', 'ProductController::delete')->middleware(['auth', 'admin'])->where_number('id');
---
+++ app/models/UsersModel.php
@@ -0,0 +1,19 @@
+<?php
+defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
+
+class UsersModel extends Model
+{
+    /**
+     * Database table this model represents.
+     *
+     * @var string
+     */
+    protected $table = 'user';
+
+    /**
+     * Primary key of the table.
+     *
+     * @var string
+     */
+    protected $primary_key = 'id';
+}
